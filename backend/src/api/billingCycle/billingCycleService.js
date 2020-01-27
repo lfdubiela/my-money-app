ee30232c4891850616b406389fcc6167761b5e06
ldubiela <ldubiela@uolinc.com>
2020-01-27 14:44:58 -0300

    Adicionando route de count do billing cycles

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,15 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((err, val) => {
+        if (err) {
+            res.status(500).json({ errors: [error] })
+            return;
+        }
+
+        res.json({ val })
+    })
+})
+
 module.exports = BillingCycle
